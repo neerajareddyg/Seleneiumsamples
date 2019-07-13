5ae59f762a836827b314abce8126c9f2016c8b3c
User <User@User-PC>
2019-07-13 14:48:11 -0700

    commiting files

--- NeeruArtifact/src/main/java/NeeruGroup/NeeruArtifact/TestFile.java
+++ NeeruArtifact/src/main/java/NeeruGroup/NeeruArtifact/TestFile.java
@@ -5,7 +5,7 @@ public class TestFile {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//testing GIT samples testing
+		//testing GIT samples testing eee 
 	}
 
 }
