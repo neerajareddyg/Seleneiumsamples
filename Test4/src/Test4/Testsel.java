6cc9b88864d6a0860f8ae99f43a6b70c77444587
neerajareddyg <User@User-PC>
2019-07-13 16:10:03 -0700

    tes commit

---
+++ Test4/src/Test4/Testsel.java
@@ -0,0 +1,23 @@
+package Test4;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.Select;
+
+
+public class Testsel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.firefox.marionette","C:\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com/");
+		//test2
+		//Testkkkkk
+		//test
+	}
+
+}
